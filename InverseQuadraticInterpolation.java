021dca6328d07b963a6aad75d75c87139bd9e403
Vlad35 <90512038+Vlad35@users.noreply.github.com>
2022-11-27 13:48:17 +0300

    Inverse quadratic interpolation

--- InverseQuadraticInterpolation.java
+++ InverseQuadraticInterpolation.java
@@ -6,14 +6,38 @@ public class Main {
     private static Scanner sc = new Scanner(System.in);
     private static double y1,y2,y3;
     private static double x1,x2,x3;
+    private static final int size = 1000;
+    private static double eps;
+    private static double[] x_arr = new double[size];
+    private static double[] y_arr = new double[size];
+    private static void InitArrays(double x,double y) {
+        double diff = (y - x) / size;
+        for (int i = 0; i < x_arr.length; i++) {
+            x_arr[i] = x + diff * i;
+            y_arr[i] = F(x_arr[i]);
+        }
+
+    }
+    private static void PrintXArr(double a,double b) {
+        InitArrays(a, b);
+        System.out.println("X array is: ");
+        for (int i = 0; i < x_arr.length; i++) {
+            System.out.println(x_arr[i]);
+        }
+    }
+    private static void PrintYArr(double a,double b) {
+        InitArrays(a, b);
+        System.out.println("Y array is: ");
+        for (int i = 0; i < y_arr.length; i++) {
+            System.out.println(y_arr[i]);
+        }
+    }
     private static double AnswerIs(double x1,double x2,double x3) {
         InitY();
         return y2*y3 / (y1 - y2) / (y1 - y3) * x1 + y1 * y3 / (y2 - y1) / (y2 - y3)*x2 + y1 * y2 / (y3 - y1) / ( y3 - y2) * x3;
     }
     private static List<Double> AllAnswers(double a, double b) {
         List<Double> list = new ArrayList<>();
-        System.out.println("Enter epsilon: ");
-        double eps = sc.nextDouble();
         double pogr = 0.000001;
         x1 = a;
         x2 = a + eps;
@@ -49,6 +73,10 @@ public class Main {
         double a = sc.nextDouble();
         System.out.println("Enter b:");
         double b = sc.nextDouble();
+        System.out.println("Enter epsilon: ");
+        eps = sc.nextDouble();
+        InitArrays(a, b);
+        System.out.print("Roots of Function are: ");
         System.out.println(AllAnswers(a, b));
     }
 }
