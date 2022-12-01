f3e8d6a727ee865f7e389ace0b0ea66523afa0c6
Vlad35 <90512038+Vlad35@users.noreply.github.com>
2022-12-01 21:15:21 +0300

    Update TrapezoidalIntegration.java

--- TrapezoidalIntegration.java
+++ TrapezoidalIntegration.java
@@ -1,10 +1,13 @@
 import java.util.Scanner;
 
 public class Main {
-    private static final int n = 100000;
+    private static final int n = 10000;
     private static double[] dots = new double[n];
     private static double f(double x) {
-        return x;
+        //return 7/(x * x + 1);
+        //return x*Math.exp(x);
+        //return Math.sin(2*x);
+        return 768 - 67*x;
     }
     private static void InitDots(double b,double a) {
         double diff = (b - a) / n;
@@ -25,16 +28,16 @@ public class Main {
     }
 
     private static double CountIntegral(double a, double b) {
-            double sum=0;
-            double[] x=new double[n];
-            double h=(b-a)/n;
-            for(int i=0;i<n;i++){
-                x[i]=a+i*h;
-            }
-            for(int i=1;i<n-1;i++){
-                sum += (f(x[i]) * (x[i + 1] - x[i - 1]))/ 2;
-            }
-            return sum;
+        double sum = 0;
+        double[] x = new double[n];
+        double h=(b - a) / n;
+        for(int i = 0;i < n;i ++){
+            x[i] = a + i * h;
+        }
+        for(int i = 1;i < n - 1;i++){
+            sum += (f(x[i]) * (x[i + 1] - x[i - 1]))/ 2;
+        }
+        return sum;
     }
 
 }
